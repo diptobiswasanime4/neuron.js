2995c9b709c23eb0a7d43589ecd240be0f37e8de
Dipto Biswas <diptobiswasanime4@gmail.com>
2025-03-03 13:36:24 +0530

    updates

---
+++ dev/xtra/betterEvolution/script.js
@@ -0,0 +1,215 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerHeight;
+canvas.height = innerHeight * 0.9;
+
+let start = true;
+
+const speed = document.getElementById("speed").value;
+const life = document.getElementById("life").value;
+const regrowth = document.getElementById("regrowth").value;
+const radius = document.getElementById("radius").value;
+
+const initGreenCount = document.getElementById("initGreenCount").value;
+const initBlueCount = document.getElementById("initBlueCount").value;
+const initRedCount = document.getElementById("initRedCount").value;
+
+const greenCount = document.getElementById("greenCountDisplay");
+const blueCount = document.getElementById("blueCountDisplay");
+const redCount = document.getElementById("redCountDisplay");
+
+const startBtn = document.getElementById("startBtn");
+
+class Particle {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.vx = color == "green" ? 0 : Math.random() * 4 - 2;
+    this.vy = color == "green" ? 0 : Math.random() * 4 - 2;
+    this.radius = 5;
+    this.life = 500;
+    this.foodConsumed = 0;
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(
+      this.x + this.radius / 2,
+      this.y + this.radius / 2,
+      this.radius / 2,
+      0,
+      Math.PI * 2
+    );
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "white";
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(
+      this.x - this.radius / 2,
+      this.y + this.radius / 2,
+      this.radius / 2,
+      0,
+      Math.PI * 2
+    );
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "white";
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(
+      this.x + this.radius / 2,
+      this.y + this.radius / 2,
+      this.radius / 4,
+      0,
+      Math.PI * 2
+    );
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(
+      this.x - this.radius / 2,
+      this.y + this.radius / 2,
+      this.radius / 4,
+      0,
+      Math.PI * 2
+    );
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+  }
+  update(ctx) {
+    this.draw(ctx);
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.color != "green") {
+      this.life -= 1;
+    }
+
+    // Applying Round World
+    if (this.x > canvas.width) {
+      this.x = 0;
+    } else if (this.x < 0) {
+      this.x = canvas.width;
+    }
+    if (this.y > canvas.height) {
+      this.y = 0;
+    } else if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  }
+}
+
+function distanceBetweenTwoParticles(p1, p2) {
+  let xDist = p2.x - p1.x;
+  let yDist = p2.y - p1.y;
+  let sqDist = Math.pow(xDist, 2) + Math.pow(yDist, 2);
+  let dist = Math.sqrt(sqDist);
+  return { dist, xDist, yDist };
+}
+
+// startBtn.addEventListener("click", () => {
+let lifeCount = {
+  greenCount: initGreenCount,
+  blueCount: initBlueCount,
+  redCount: initRedCount,
+};
+
+let particles = [];
+
+for (let i = 0; i < lifeCount.greenCount; i++) {
+  let x = Math.random() * canvas.width;
+  let y = Math.random() * canvas.height;
+  particles.push(new Particle(x, y, "green"));
+}
+
+for (let i = 0; i < lifeCount.blueCount; i++) {
+  let x = Math.random() * canvas.width;
+  let y = Math.random() * canvas.height;
+  particles.push(new Particle(x, y, "blue"));
+}
+
+for (let i = 0; i < lifeCount.redCount; i++) {
+  x = Math.random() * canvas.width;
+  y = Math.random() * canvas.height;
+  particles.push(new Particle(x, y, "red"));
+}
+
+setInterval(() => {
+  let x = Math.random() * canvas.width;
+  let y = Math.random() * canvas.height;
+  particles.push(new Particle(x, y, "green"));
+  lifeCount.greenCount++;
+}, 100);
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  console.log(lifeCount);
+
+  greenCount.textContent = lifeCount.greenCount;
+  blueCount.textContent = lifeCount.blueCount;
+  redCount.textContent = lifeCount.redCount;
+
+  for (let i1 = particles.length - 1; i1 >= 0; i1--) {
+    let p1 = particles[i1];
+    p1.update(ctx);
+
+    if (p1.life <= 0) {
+      particles.splice(i1, 1);
+      if (p1.color == "blue") {
+        lifeCount.blueCount--;
+      } else if (p1.color == "green") {
+        lifeCount.greenCount--;
+      } else if (p1.color == "red") {
+        lifeCount.redCount--;
+      }
+      continue;
+    }
+
+    for (let i2 = particles.length - 1; i2 >= 0; i2--) {
+      if (i1 === i2) continue;
+      let p2 = particles[i2];
+
+      let dist = distanceBetweenTwoParticles(p1, p2);
+      if (dist.dist < p1.radius) {
+        if (p1.color == "blue" && p2.color == "green") {
+          particles.splice(i2, 1);
+          particles.push(new Particle(p1.x, p1.y, "blue"));
+
+          lifeCount.greenCount--;
+          lifeCount.blueCount++;
+
+          p1.life = 500;
+          p1.foodConsumed = 0;
+        } else if (p1.color == "blue" && p2.color == "red") {
+          particles.splice(i1, 1);
+          lifeCount.blueCount--;
+
+          p2.life = 500;
+          p2.foodConsumed++;
+
+          if (p2.foodConsumed == 3) {
+            particles.push(new Particle(p2.x, p2.y, "red"));
+            lifeCount.redCount++;
+            p2.foodConsumed = 0;
+          }
+          break;
+        }
+      }
+    }
+  }
+}
+
+animate();
+// });
--- dev/xtra/conway/script.js
+++ dev/xtra/conway/script.js
@@ -4,25 +4,77 @@ const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 
-const GRID_SIDE = 10;
+const GRID_SIDE = 20;
 const GRID_ROWS = canvas.height / GRID_SIDE;
 const GRID_COLS = canvas.width / GRID_SIDE;
 
 const nextTurn = document.getElementById("next-turn");
 const start = document.getElementById("start");
 
-nextTurn.addEventListener("click", (e) => {
-  updateGrid();
-
-  // draw
-  drawGrid();
-});
-
 let grid = [];
 let nextGrid = [];
 
 initializeGrid();
 drawGrid();
+
+function initializeGrid() {
+  for (let i = 0; i < GRID_COLS; i++) {
+    grid[i] = [];
+    for (let j = 0; j < GRID_ROWS; j++) {
+      let rand = Math.random();
+      grid[i][j] = rand > 0.7 ? 1 : 0;
+    }
+  }
+}
+
+console.log(grid);
+
+function drawGrid() {
+  for (let i = 0; i < GRID_COLS; i++) {
+    for (let j = 0; j < GRID_ROWS; j++) {
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
+      ctx.stroke();
+      if (grid[i][j] == 1) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
+      } else {
+        ctx.fillStyle = "black";
+        ctx.fillRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
+      }
+      ctx.closePath();
+    }
+  }
+}
+
+nextTurn.addEventListener("click", (e) => {
+  updateGrid();
+  drawGrid();
+});
+
+function updateGrid() {
+  nextGrid = grid.map((row) => [...row]);
+  for (let i = 0; i < GRID_COLS; i++) {
+    for (let j = 0; j < GRID_ROWS; j++) {
+      // living cells die
+      let neighbors = neighborCount(i, j);
+      if (grid[i][j] == 1) {
+        if (neighbors < 2 || neighbors > 3) {
+          nextGrid[i][j] = 0;
+        }
+      }
+      // reproduction
+      if (grid[i][j] == 0) {
+        if (neighbors == 3) {
+          nextGrid[i][j] = 1;
+        }
+      }
+    }
+  }
+  grid = nextGrid.map((row) => [...row]);
+}
 
 function neighborCount(i, j) {
   let count = 0;
@@ -60,62 +112,9 @@ function neighborCount(i, j) {
   return count;
 }
 
-function initializeGrid() {
-  for (let i = 0; i < GRID_COLS; i++) {
-    grid[i] = [];
-    for (let j = 0; j < GRID_ROWS; j++) {
-      let rand = Math.random();
-      grid[i][j] = rand > 0.7 ? 1 : 0;
-    }
-  }
-}
-
-function drawGrid() {
-  for (let i = 0; i < GRID_COLS; i++) {
-    for (let j = 0; j < GRID_ROWS; j++) {
-      ctx.beginPath();
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = "white";
-      ctx.strokeRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
-      ctx.stroke();
-      if (grid[i][j] == 1) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
-      } else {
-        ctx.fillStyle = "black";
-        ctx.fillRect(j * GRID_SIDE, i * GRID_SIDE, GRID_SIDE, GRID_SIDE);
-      }
-      ctx.closePath();
-    }
-  }
-}
-
-function updateGrid() {
-  nextGrid = grid.map((row) => [...row]);
-  for (let i = 0; i < GRID_COLS; i++) {
-    for (let j = 0; j < GRID_ROWS; j++) {
-      // living cell dies
-      if (grid[i][j] == 1) {
-        if (neighborCount(i, j) < 2 || neighborCount(i, j) > 3) {
-          nextGrid[i][j] = 0;
-        }
-      }
-
-      // reproduction
-      if (grid[i][j] == 0) {
-        if (neighborCount(i, j) == 3) {
-          nextGrid[i][j] = 1;
-        }
-      }
-    }
-  }
-
-  grid = nextGrid.map((row) => [...row]);
-}
-
 start.addEventListener("click", (e) => {
   setInterval(() => {
     updateGrid();
     drawGrid();
-  }, 250);
+  }, [250]);
 });
